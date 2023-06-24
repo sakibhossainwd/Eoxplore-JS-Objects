1dd22b837c9ac3f14114791e844ee32f2aece752
sakibhossain <sakibhossainceo@gmail.com>
2023-06-23 17:26:37 -0700

    added

--- objects.js
+++ objects.js
@@ -5,7 +5,20 @@ player.name = 'Sakib Hossain';
 player.specialty = 'batsman';
 // Method
 player.bat = function(){ 
-    console.log('swing your bat');
-}
+//     console.log('swing your bat');
+// }
 player.bat();
-console.log(player);
+// console.log(player);
+
+const student = {
+    name: 'Rakib',
+    class: 'five',
+    roll: function(){
+        // console.log('05')
+    },
+    grade: 'A+'
+}
+
+// object constructor
+const person = new Object();
+console.log(person);
