d900e36e8bacdf07eeca54059f7e6bcf1f2440e5
sakibhossain <sakibhossainceo@gmail.com>
2023-06-24 06:50:58 -0700

    aded

---
+++ freeze.js
@@ -0,0 +1,7 @@
+const botte = {color: 'yellow', price: 500, isCleaned: true, capacity: 1};
+// get all properties/key word in array
+const keys = Object.keys(botte);
+console.log(keys);
+// get all properties value in array
+const values = Object.values(botte);
+console.log(values);
