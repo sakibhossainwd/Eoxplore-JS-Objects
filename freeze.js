8f0ae857fbe35bc94152cf2575c9750086005f69
sakibhossain <sakibhossainceo@gmail.com>
2023-06-24 07:16:45 -0700

    adde

--- freeze.js
+++ freeze.js
@@ -8,9 +8,13 @@ const values = Object.values(botte);
 // get sigle properties and value array in array / two dimentional array
 const pair = Object.entries(botte);
 // console.log(pair);
-// seal means fixt the object 
+// seal means fixt the object now we can't delete any properties and can't add new properties but can modifi
 Object.seal(botte);
+// modifi properties value
+botte.price = 200;
+// add new properties
+botte.height = 100;
 // delete properties and value by using Object.delete
-delete botte.isCleaned;
+// delete botte.isCleaned;
 
 console.log(botte);
