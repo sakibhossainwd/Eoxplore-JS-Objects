1634bbc238c61cf28bf54bb79f8dfe26a65b6d04
sakibhossain <sakibhossainceo@gmail.com>
2023-06-24 09:22:21 -0700

    added

--- loop.js
+++ loop.js
@@ -8,7 +8,26 @@ const numbers = [12, 20, 52, 59];
 const botte = {color: 'yellow', price: 500, isCleaned: true, capacity: 1};
 // first option to loop through an object
 const keys = Object.keys(botte);
-console.log(keys);
+// console.log(keys);
+/*
+3. ways to read object properties
+ 3.1- bottle.color
+ 3.2- bottle['color']
+ 3.3- bottle[key]
+*/
 for(const key of keys){
-    console.log(key);
+    // console.log(key, botte[key]);
+}
+
+// for in loop
+for(const key in botte){
+    const value = botte[key];
+    // console.log(key, value);
+}
+
+// Advanced
+const pair = Object.entries(botte);
+console.log(pair);
+for(const [key, value] of Object.entries(botte)){
+    console.log(key, value);
 }
